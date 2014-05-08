1c6bd2c3dc15bcfd29b0a2441a7e70315b65581a
AliceCengal <cengallut@gmail.com>
2014-05-08 18:14:24 -0500

    State Machine for UI transition

--- src/edu/vanderbilt/doreguide/MainActivity.scala
+++ src/edu/vanderbilt/doreguide/MainActivity.scala
@@ -1,18 +1,22 @@
 package edu.vanderbilt.doreguide
 
 import android.os.{Message, Handler, Bundle}
-import android.app.{FragmentManager, Fragment, Activity}
+import android.app.{FragmentTransaction, FragmentManager, Fragment, Activity}
 import android.view.{MenuItem, Menu}
 
 import edu.vanderbilt.doreguide.service.{HandlerActor, EventBus}
 import edu.vanderbilt.doreguide.view.SimpleInjections
+import edu.vanderbilt.doreguide.MainActivity._
+import edu.vanderbilt.doreguide.model.Place
 
 class MainActivity extends Activity
                            with SimpleInjections.ActivityInjection
                            with Handler.Callback {
 
   lazy val communicator = HandlerActor.sync(this)
-  var currentFragment: Fragment = null
+  //var currentFragment: Fragment = null
+  implicit val implicitMain = this
+  var mainState: MainState = null
 
   override def onCreate(saved: Bundle) {
     super.onCreate(saved)
@@ -112,6 +116,308 @@ class MainActivity extends Activity
       case _ =>
     }
     true
+  }
+
+  def clearFragments() {
+    getFragmentManager.
+        beginTransaction().
+        remove(getFragmentManager.findFragmentById(R.id.main_main)).
+        remove(getFragmentManager.findFragmentById(R.id.main_underbar)).
+        commit()
+  }
+
+  def superBack() {
+    super.onBackPressed()
+  }
+
+  def transaction(action: FragmentTransaction => Unit) {
+    val ft = getFragmentManager.beginTransaction()
+    action(ft)
+    ft.commit()
+  }
+
+}
+
+object MainActivity {
+
+  case object HeartMenuItem
+  case object BackButton
+  case object HomeUp
+  case object SettingMenuItem
+
+  trait MainState {
+    def enter(implicit main: MainActivity)
+    def handleEvent(event: AnyRef)(implicit main: MainActivity)
+    def leave(implicit main: MainActivity)
+  }
+
+  object ViewingCurrentLocation extends MainState {
+
+    private var frag: Fragment = null
+
+    def enter(implicit main: MainActivity) {
+      main.mainState = this
+      frag = PlaceDetailFrag.showNearestPlace
+      main.transaction(_.add(R.id.main_main, frag))
+    }
+
+    def handleEvent(event: AnyRef)(implicit main: MainActivity): Unit = {
+      event match {
+        case BackButton =>
+          frag = null
+          main.superBack()
+        case HeartMenuItem =>
+          this.leave
+          ViewingHearted.enter
+        case PlaceDetailFrag.MapButtonClicked(plc) =>
+          this.leave
+          ViewingMap.enter
+        case _ =>
+      }
+    }
+
+    def leave(implicit main: MainActivity): Unit = {
+      if (frag != null) {
+        main.transaction(_.remove(frag))
+        frag = null
+      }
+    }
+  }
+
+  object ViewingHearted extends MainState {
+    private var frag: Fragment = null
+
+    def enter(implicit main: MainActivity): Unit = {
+      main.mainState = this
+      frag = new HeartFrag
+      main.transaction(_.add(R.id.main_main, frag))
+    }
+
+    def handleEvent(event: AnyRef)(implicit main: MainActivity): Unit = {
+      event match {
+        case BackButton =>
+          this.leave
+          ViewingCurrentLocation.enter
+        case HeartFrag.MapButtonClicked =>
+          this.leave
+          ViewingHeartedMap.enter
+        case HeartFrag.ListItemClicked(plc) =>
+          this.leave
+          ViewingPlaceFromHearted.enter
+          ViewingPlaceFromHearted.handleEvent(plc)
+        case _          =>
+      }
+    }
+
+    def leave(implicit main: MainActivity): Unit = {
+      if (frag != null) {
+        main.transaction(_.remove(frag))
+        frag = null
+      }
+    }
+  }
+
+  object ViewingMap extends MainState {
+    private var frag: Fragment = null
+    private var fragUnder: Fragment = null
+
+    def enter(implicit main: MainActivity): Unit = {
+      main.mainState = this
+      frag = PlacesMapFragment.showAll
+      fragUnder = new MapUnderbarFrag
+      main.transaction(_.
+                       add(R.id.main_main, frag).
+                       add(R.id.main_underbar, fragUnder))
+    }
+
+    def handleEvent(event: AnyRef)(implicit main: MainActivity): Unit = {
+      event match {
+        case BackButton =>
+          this.leave
+          ViewingCurrentLocation.enter
+        case MapUnderbarFrag.MapUnderbarClicked(plc) =>
+          this.leave
+          ViewingPlaceFromMap.enter
+          ViewingPlaceFromMap.handleEvent(plc)
+        case _ =>
+      }
+    }
+
+    def leave(implicit main: MainActivity): Unit = {
+      main.transaction{ ft =>
+        if (frag != null) {
+          ft.remove(frag)
+          frag = null
+        }
+        if (fragUnder != null) {
+          ft.remove(fragUnder)
+          fragUnder = null
+        }
+      }
+    }
+  }
+
+  object ViewingPlaceFromMap extends MainState {
+    private var frag: Fragment = null
+
+    def enter(implicit main: MainActivity): Unit = {
+      main.mainState = this
+    }
+
+    def handleEvent(event: AnyRef)(implicit main: MainActivity): Unit = {
+      event match {
+        case plc: Place =>
+          frag = PlaceDetailFrag.showThisPlace(plc)
+          main.transaction(_.add(R.id.main_main, frag))
+        case PlaceDetailFrag.NearbyPlaceSelected(plc) =>
+          frag = PlaceDetailFrag.showThisPlace(plc)
+          main.transaction(_.replace(R.id.main_main, frag))
+        case BackButton | PlaceDetailFrag.MapButtonClicked(_) =>
+          this.leave
+          ViewingMap.enter
+        case _ =>
+      }
+    }
+
+    def leave(implicit main: MainActivity): Unit = {
+      if (frag != null) {
+        main.transaction(_.remove(frag))
+        frag = null
+      }
+    }
+  }
+
+  object ViewingPlaceFromCurrent extends MainState {
+    private var frag: Fragment = null
+
+    def enter(implicit main: MainActivity): Unit = {
+      main.mainState = this
+    }
+
+    def handleEvent(event: AnyRef)(implicit main: MainActivity): Unit = {
+      event match {
+        case plc: Place =>
+          frag = PlaceDetailFrag.showThisPlace(plc)
+          main.transaction(_.add(R.id.main_main, frag))
+        case PlaceDetailFrag.NearbyPlaceSelected(plc) =>
+          frag = PlaceDetailFrag.showThisPlace(plc)
+          main.transaction(_.replace(R.id.main_main, frag))
+        case PlaceDetailFrag.MapButtonClicked(_) =>
+          this.leave
+          ViewingMap.enter
+        case BackButton =>
+          this.leave
+          ViewingCurrentLocation.enter
+        case _ =>
+      }
+    }
+
+    def leave(implicit main: MainActivity): Unit = {
+      if (frag != null) {
+        main.transaction(_.remove(frag))
+        frag = null
+      }
+    }
+  }
+
+  object ViewingHeartedMap extends MainState {
+    private var frag: Fragment = null
+    private var fragUnder: Fragment = null
+
+    def enter(implicit main: MainActivity): Unit = {
+      main.mainState = this
+      frag = PlacesMapFragment.showHearted
+      fragUnder = new MapUnderbarFrag
+      main.transaction(_.
+                       add(R.id.main_main, frag).
+                       add(R.id.main_underbar, frag))
+    }
+
+    def handleEvent(event: AnyRef)(implicit main: MainActivity): Unit = {
+      event match {
+        case MapUnderbarFrag.MapUnderbarClicked(plc) =>
+          this.leave
+          ViewingPlaceFromHeartedMap.enter
+          ViewingPlaceFromHeartedMap.handleEvent(plc)
+        case BackButton =>
+          this.leave
+          ViewingHearted.enter
+        case _ =>
+      }
+    }
+
+    def leave(implicit main: MainActivity): Unit = {
+      main.transaction{ ft =>
+        if (frag != null) ft.remove(frag)
+        if (fragUnder != null) ft.remove(fragUnder)
+
+        frag = null
+        fragUnder = null
+      }
+    }
+  }
+
+  object ViewingPlaceFromHeartedMap extends MainState {
+    private var frag: Fragment = null
+
+    def enter(implicit main: MainActivity): Unit = {
+      main.mainState = this
+    }
+
+    def handleEvent(event: AnyRef)(implicit main: MainActivity): Unit = {
+      event match {
+        case plc: Place =>
+          frag = PlaceDetailFrag.showThisPlace(plc)
+          main.transaction(_.add(R.id.main_main, frag))
+        case PlaceDetailFrag.NearbyPlaceSelected(plc) =>
+          frag = PlaceDetailFrag.showThisPlace(plc)
+          main.transaction(_.replace(R.id.main_main, frag))
+        case BackButton | PlaceDetailFrag.MapButtonClicked(_) =>
+          this.leave
+          ViewingHeartedMap.enter
+        case _ =>
+      }
+    }
+
+    def leave(implicit main: MainActivity): Unit = {
+      if (frag != null) {
+        main.transaction(_.remove(frag))
+        frag = null
+      }
+    }
+  }
+
+  object ViewingPlaceFromHearted extends MainState {
+    private var frag: Fragment = null
+
+    def enter(implicit main: MainActivity): Unit = {
+      main.mainState = this
+    }
+
+    def handleEvent(event: AnyRef)(implicit main: MainActivity): Unit = {
+      event match {
+        case plc: Place =>
+          frag = PlaceDetailFrag.showThisPlace(plc)
+          main.transaction(_.add(R.id.main_main, frag))
+        case PlaceDetailFrag.NearbyPlaceSelected(plc) =>
+          frag = PlaceDetailFrag.showThisPlace(plc)
+          main.transaction(_.replace(R.id.main_main, frag))
+        case BackButton =>
+          this.leave
+          ViewingHearted.enter
+        case PlaceDetailFrag.MapButtonClicked(_) =>
+          this.leave
+          ViewingHeartedMap.enter
+        case _ =>
+      }
+    }
+
+    def leave(implicit main: MainActivity): Unit = {
+      if (frag != null) {
+        main.transaction(_.remove(frag))
+        frag = null
+      }
+    }
   }
 
 }
